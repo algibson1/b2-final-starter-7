d50a0ed76848bb4fc98ae24c51c191babb74adbc
Artemy Gibson <artemygibson@gmail.com>
2023-08-03 18:42:24 -0700

    Add validation for maximum discount of 99 percent

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,7 @@
 class BulkDiscount < ApplicationRecord
   validates_presence_of :percentage, :quantity
+  validates :percentage, numericality: { less_than_or_equal_to: 99 }
+  validates_numericality_of :quantity
 
   belongs_to :merchant
 end
