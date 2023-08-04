5476f8a0895d8ccb1b29555097b1c6e98beb51cd
Artemy Gibson <artemygibson@gmail.com>
2023-08-03 18:48:40 -0700

    Test validation for percentage being less than or equal to 99

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe BulkDiscount do
   describe "validations" do
     it {should validate_presence_of :quantity}
     it {should validate_presence_of :percentage}
+    it { should validate_numericality_of :quantity }
+    it {should validate_numericality_of(:percentage).is_less_than_or_equal_to(99) }
   end
 
   describe "relationships" do
