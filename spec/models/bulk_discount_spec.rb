c480b08c861a61647c0057023cd6b4e36618ae16
Artemy Gibson <artemygibson@gmail.com>
2023-08-05 21:05:29 -0700

    Test invoice_items relationship

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -10,22 +10,7 @@ RSpec.describe BulkDiscount do
 
   describe "relationships" do
     it {should belong_to :merchant}
+    it {should have_many :invoice_items }
   end
-
-  # it "kicks in when enough items are ordered" do
-  #   # need one merchant, one item, one bulk discount, one invoice, n - 1 invoice items
-  #   # check that unit_price on invoice_items matches unit_price on items 
-  #   # add one more invoice_items
-  #   # check that price in invoice_items is now discounted
-  # end
-
-  # it "is eligible for all items a merchant sells" do
-  #   # need one merchant, two items, one bulk discount, one invoice, n-1 invoice_items per item
-  #   # check that each item's unit_price is same as item unit_price
-  #   # add one more invoice_item for first item
-  #   # check that first item has been discounted, but second has not
-  #   # add another invoice_item for second item
-  #   # check both are discounted
-  # end
 
 end
