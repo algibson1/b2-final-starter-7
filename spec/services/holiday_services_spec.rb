3bd063106512fd5bd5a0edcc9c618904006ac1d8
Artemy Gibson <artemygibson@gmail.com>
2023-08-07 14:45:55 -0700

    Remove comments

--- spec/services/holiday_services_spec.rb
+++ spec/services/holiday_services_spec.rb
@@ -24,13 +24,3 @@ RSpec.describe HolidayService do
     expect(dates[1] < dates[2]).to eq(true)
   end
 end
-
-#   As a merchant
-# When I visit the discounts index page
-# I see a section with a header of "Upcoming Holidays"
-# In this section the name and date of the next 3 upcoming US holidays are listed.
-
-# Use the Next Public Holidays Endpoint in the [Nager.Date API](https://date.nager.at/swagger/index.html)
-
-
-# test: the service, the PORO, the view
